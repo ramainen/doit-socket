6f8c6c7088eb190734d136701b967697ae92b8db
damir <ainu.sky@gmail.com>
2014-08-30 13:50:21 +0400

    Initial

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var url = require('url');
+
+var sockets_array=[];
+
+
+app.listen(32800);
+
+
+function handler (req, res) {
+
+
+var query =url.parse(req.url, true).query;
+
+if((typeof (query.id) != 'undefined') &&  (typeof (query.message) != 'undefined') && (typeof(sockets_array[query.id]) != 'undefined')){
+     sockets_array[query.id].emit(query.message, query);
+ }
+
+  res.writeHead(200);
+  res.end("OK");
+
+}
+
+
+io.on('connection', function (socket) {
+  //socket.emit('news', { hello: 'world' });
+
+   socket.on('register', function (data) {
+    console.log(data.userid);
+        sockets_array[data.userid] = socket;
+  });
+});
