07e8c0ecc05be8e9aeed757009676c1e1615339c
damir <ainu.sky@gmail.com>
2014-08-30 14:53:39 +0400

    Run!

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var app = require('http').createServer(handler)
-//var io = require('socket.io')(app);
-//var fs = require('fs');
-//var url = require('url');
+var io = require('socket.io')(app);
+var url = require('url');
 
-//var sockets_array=[];
+var sockets_array=[];
 
 
 app.listen(process.env.PORT || 1337);
@@ -12,18 +11,17 @@ app.listen(process.env.PORT || 1337);
 function handler (req, res) {
 
 
-//var query =url.parse(req.url, true).query;
-/*
+var query =url.parse(req.url, true).query;
+
 if((typeof (query.id) != 'undefined') &&  (typeof (query.message) != 'undefined') && (typeof(sockets_array[query.id]) != 'undefined')){
      sockets_array[query.id].emit(query.message, query);
- }
-*/
-  res.writeHead(200);
-  res.end("OK");
-
 }
 
-/*
+  res.writeHead(200);
+  res.end("OK");
+}
+
+
 io.on('connection', function (socket) {
   //socket.emit('news', { hello: 'world' });
 
@@ -32,4 +30,3 @@ io.on('connection', function (socket) {
         sockets_array[data.userid] = socket;
   });
 });
-*/
